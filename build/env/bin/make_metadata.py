cea9a06ec220b6e2d7b3e1597d8d395e160f450b
inx2016 <352322399@qq.com>
2019-11-24 00:05:32 +0800

    myhue

---
+++ build/env/bin/make_metadata.py
@@ -0,0 +1,4 @@
+#!/home/inx/PycharmProjects/hue/build/env/bin/python2.7
+# EASY-INSTALL-SCRIPT: 'pysaml2==4.4.0','make_metadata.py'
+__requires__ = 'pysaml2==4.4.0'
+__import__('pkg_resources').run_script('pysaml2==4.4.0', 'make_metadata.py')
---
+++ build/env/bin/rst2odt_prepstyles.py
@@ -0,0 +1,4 @@
+#!/home/inx/PycharmProjects/hue/build/env/bin/python2.7
+# EASY-INSTALL-SCRIPT: 'docutils==0.14','rst2odt_prepstyles.py'
+__requires__ = 'docutils==0.14'
+__import__('pkg_resources').run_script('docutils==0.14', 'rst2odt_prepstyles.py')
